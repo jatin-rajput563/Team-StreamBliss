1d3059ae8c1a458467eb43aace4848dc6a2580ad
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-11 09:06:08 +0530

    change in custom btn

--- src/components/common/CustomButton.jsx
+++ src/components/common/CustomButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CustomButton = ({ btnClass, btnText }) => {
     return (
-        <button className={`custom-gradient-border font-semibold text-lg leading-[160%] text-[#B3B3B3] rounded-[65px] border transition-all duration-200 ease-linear hover:border-transparent hover:text-white hover:bg-gradient-to-b from-[#B851E0] to-[#EB489B] ${btnClass}`}>
+        <button className={`font-semibold text-lg leading-[160%] text-[#B3B3B3] rounded-[65px] border transition-all duration-200 ease-linear hover:border-transparent hover:text-white hover:bg-gradient-to-b from-[#B851E0] to-[#EB489B] ${btnClass}`}>
             {btnText}
         </button>
     )
