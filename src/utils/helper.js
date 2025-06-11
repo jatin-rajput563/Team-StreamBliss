138c82ae0b609dc7a62802846fca5b5f14ce97d2
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-11 10:30:17 +0530

    header done

---
+++ src/utils/helper.js
@@ -0,0 +1,18 @@
+export const HEADER_LIST = [
+    {
+        title: "Home",
+        link: "#home"
+    },
+    {
+        title: "How it Works",
+        link: "#work"
+    },
+    {
+        title: "Features",
+        link: "#features"
+    },
+    {
+        title: "Pricing",
+        link: "#pricing"
+    },
+]
