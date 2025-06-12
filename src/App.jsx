02aa1342231bd90a5cd191822f2923f07dc2a1bc
jatin-rajput563 <jkt3298@gmail.com>
2025-06-12 11:27:22 +0530

    pricing-section-complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,43 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Action from "./components/Action";
 import Everything from "./components/Everything";
-// import Pricing from "./components/Pricing";
+import Pricing from "./components/Pricing";
 import CompareFeature from "./components/CompareFeature";
 import Members from "./components/Members";
 import Ready from "./components/Ready";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MonthlyPricing from "./components/MonthlyPricing";
+import YearlyPricing from "./components/YearlyPricing";
 
 function App() {
   return (
-    <>
-      <Hero />
-      <Action />
-      <Everything />
-      {/* <Pricing /> */}
-      <CompareFeature />
-      <Members />
-      <Ready />
-      <Faq />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Home Route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Action />
+              <Everything />
+              <Pricing />
+              <CompareFeature />
+              <Members />
+              <Ready />
+              <Faq />
+              <Footer />
+            </>
+          }
+        />
+        {/* Monthly Pricing Route */}
+        <Route path="/monthlyPricing" element={<MonthlyPricing />} />
+        {/* Yearly Pricing Route */}
+        <Route path="/yearlyPricing" element={<YearlyPricing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
